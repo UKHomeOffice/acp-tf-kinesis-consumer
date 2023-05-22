66816e3ba1f1ba8d4eb3e52082f924b8ad004389
Chris Freeman <chris.freeman@digital.homeoffice.gov.uk>
2023-05-22 11:58:24 +0100

    v2 - remove IAM

--- datasource.tf
+++ datasource.tf
@@ -1,8 +1,3 @@
 data "aws_region" "current" {}
 
 data "aws_caller_identity" "current" {}
-
-data "aws_ssm_parameter" "kinesis_users_public_key" {
-  name            = var.kinesis_users_public_key_parameter_name
-  with_decryption = true
-}
